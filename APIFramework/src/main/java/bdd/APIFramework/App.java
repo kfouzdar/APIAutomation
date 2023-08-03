01c12f260e123c7d294309b3e7d248abf5454170
kfouzdar <keshabf@outlook.com>
2023-08-04 01:42:54 +0530

    New commit

---
+++ APIFramework/src/main/java/bdd/APIFramework/App.java
@@ -0,0 +1,13 @@
+package bdd.APIFramework;
+
+/**
+ * API Automation!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "API Automation" );
+    }
+}
